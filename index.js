538aeafd0a6c687d4e86073a8f20a9a33ba21ae1
unknown <mdmehedyh55@gamil.com>
2022-03-25 18:29:50 +0600

    intall and bacis

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ucfjq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+console.log(uri)
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
